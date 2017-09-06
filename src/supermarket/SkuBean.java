60fa475bf0bd857964080c41851d898b5af248ce
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-09-06 10:15:43 +0100

    create SkuBean class with instance variables

--- src/supermarket/SkuBean.java
+++ src/supermarket/SkuBean.java
@@ -3,5 +3,12 @@ package supermarket;
 import java.io.Serializable;
 
 public class SkuBean implements Serializable {
-
+	
+	private String productCode;
+	private String description;
+	private BigDecimal price;
+	
+	public SkuBean() {
+		
+	}
 }
